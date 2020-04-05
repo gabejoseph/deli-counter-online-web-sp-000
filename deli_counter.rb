4b18065f234668a5343e535ddff7e879ef758354
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-05 15:41:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    x = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      x << " #{index+1}. #{name}"
+    end
+    puts x
+  end
+end
+
+def take_a_number(katz_deli)
+  katz_deli.unshift(x)
+  puts "Welcome, #{katz_deli[0]}. You are number 1 in line."
+end
+
+
+# def now_serving(katz_deli)
+#   #if noone in line "There is nobody waiting to be served"
+#   #puts next person in line
+#   #remove person from front of line
+# end
